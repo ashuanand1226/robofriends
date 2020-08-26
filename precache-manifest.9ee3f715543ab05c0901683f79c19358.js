6198d840eada08f34f67498844c5554e6086bb8e
Ashu Anand <ashuanand1226@gmail.com>
2020-08-26 01:07:43 -0500

    Updates

---
+++ precache-manifest.9ee3f715543ab05c0901683f79c19358.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee5ca395fc26cc8ec707c3394f997e1",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ca626df3841da2c19152",
+    "url": "/robofriends/static/css/main.c55727d1.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca626df3841da2c19152",
+    "url": "/robofriends/static/js/main.b26a69f7.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
